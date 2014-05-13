e73844ec025b56933fc70243508ac8e48507c682
Tyler Shimko <TShimko126@gmail.com>
2014-05-13 11:03:41 -0500

    Add lines to incomplete files

--- PlateStitcher.py
+++ PlateStitcher.py
@@ -33,6 +33,12 @@ def nameFile(f):
     return directory + "/" + number + "_" + drug + "_stitched.txt"
 
 
+def nameFile2(f):
+    number = f.split("_")[0]
+    drug = f.split("_")[1].split(".")[0]
+    return directory + "/" + number + "_" + drug + "_complete.txt"
+
+
 # Get the duplicate plate numbers
 duplicates = [[n, times] for n, times in collections.Counter(numbers).items() if times > 1]
 
@@ -41,6 +47,18 @@ if len(duplicates) != 0:
     if not exists(folderName):
         mkdir(folderName)
 
+incFolderName = directory + "/IncompleteData"
+if not exists(incFolderName):
+    mkdir(incFolderName)
+
+allRows = ["A", "B", "C", "D", "E", "F", "G", "H"]
+allCols = range(1, 13)
+allWells = Set()
+for n in range(0, len(allRows)):
+    for p in range(0, len(allCols)):
+        allWells.add((allRows[n], str(allCols[p])))
+
+# Handle the duplicate files
 for i in range(0, len(duplicates)):
     skip = False
     drugNames = Set()
@@ -82,12 +100,6 @@ for i in range(0, len(duplicates)):
                     well = k[2], k[3]
                     wells.add(well)
 
-            allRows = ["A", "B", "C", "D", "E", "F", "G", "H"]
-            allCols = range(1, 13)
-            allWells = Set()
-            for n in range(0, len(allRows)):
-                for p in range(0, len(allCols)):
-                    allWells.add((allRows[n], str(allCols[p])))
             missingWells = allWells.difference(wells)
 
             for q in missingWells:
@@ -95,7 +107,6 @@ for i in range(0, len(duplicates)):
                           "-1", "0", "-1", "-1", "-1", "-1", "-1",
                           "-1", "-1", "-1", "-1", "-1", "-1", "-1",
                           "-1", "-1", "-1", "-1", "-1", "-1", "-1"]
-                # newRow = ("\t".join(newrow) + "\n")
                 allData.append(newRow)
 
             newFile = open(name, "w+")
@@ -105,3 +116,51 @@ for i in range(0, len(duplicates)):
                 newFile.write("\t".join(l) + "\n")
 
             newFile.close()
+
+            print ("Stitched " + str(duplicates[i][1]) + " " +
+                   str(duplicates[i][1]) + " entries together and added dummy data for " +
+                   str(len(missingWells)) + " missing wells. Original files moved to " +
+                   folderName + ".\n")
+
+# Check for the presence of every well in all of the files
+for i in range(0, len(txtFiles)):
+    fileName = directory + "/" + txtFiles[i]
+    allData = []
+    wells = Set()
+    with open(fileName, "r") as f:
+        reader = csv.reader(f, delimiter="\t")
+        header = reader.next()
+        allData.append(header)
+        for row in reader:
+            if len(row) == len(header):
+                allData.append(row)
+        for k in allData:
+            if k != header:
+                well = k[2], k[3]
+                wells.add(well)
+
+        missingWells = allWells.difference(wells)
+
+        if len(missingWells) == 0:
+            shutil.move(fileName, incFolderName)
+
+            for q in missingWells:
+                newRow = ["-1", "-1", str(q[0]), str(q[1]), "N",
+                          "-1", "0", "-1", "-1", "-1", "-1", "-1",
+                          "-1", "-1", "-1", "-1", "-1", "-1", "-1",
+                          "-1", "-1", "-1", "-1", "-1", "-1", "-1"]
+                allData.append(newRow)
+
+            name = nameFile2(txtFiles[i])
+
+            newFile = open(name, "w+")
+            newFile.write("\t".join(header) + "\n")
+
+            for l in allData:
+                newFile.write("\t".join(l) + "\n")
+
+            newFile.close()
+
+        print ("Added " + str(len(missingWells)) + " lines to " + txtFiles[i] +
+               " to create " + name + ". Original file hase been moved to " +
+               incFolderName + ".\n")
