2b740332e3f2da8aee2b06f405f4471a6bd3ffde
Tyler Shimko <TShimko126@gmail.com>
2014-06-27 17:01:08 -0500

    Add working regression code

--- SimpleDataProcess.R
+++ SimpleDataProcess.R
@@ -22,6 +22,7 @@ info <- function(filePath, levels = 1){
     return(frame)
 }
 
+#Process the setup plates
 procSetup <- function(file, tofmin=0, tofmax=2000, extmin=20, extmax=5000) {
     
     #Read in the sorter data from the file
@@ -37,6 +38,37 @@ procSetup <- function(file, tofmin=0, tofmax=2000, extmin=20, extmax=5000) {
     return(proc)
 }
 
+regress <- function(data, completeData, controls){
+    plates <- data[!duplicated(data[,c("assay", "plate", "drug")]), c("assay", "plate", "drug")]
+    controlValues <- plates %>%
+        group_by(assay, plate, drug) %>%
+        do(data.frame(filter(completeData,
+                                       assay==as.character(.$assay[1]),
+                                       as.numeric(plate) %in% as.numeric(controls[sapply(controls$plates,
+                                                                                         function(x){as.numeric(.$plate[1]) %in% as.numeric(x)}), "control"])) %>%
+                                    group_by(row, col) %>%
+                                    summarise_each(funs(mean(., na.rm=TRUE)), -date, -experiment, -round, -assay, -plate, -drug))},
+                    error = function(err){return(data.frame(matrix(nrow=96)))}))
+    
+    regressedValues <- lapply(which(colnames(data)=="n"):ncol(data),
+                              function(x){
+                                  tryCatch({residuals(lm(data[,x] ~ data$assay + controlValues[,which(colnames(controlValues)==colnames(data)[x])], na.action=na.exclude))},
+                                           error = function(err){return(NA)})
+                              })
+    finalDF <- data.frame(data, regressedValues)
+    colnames(finalDF)[(which(colnames(finalDF)=="norm.n")+1):ncol(finalDF)] <- paste0("resid.", colnames(finalDF)[which(colnames(finalDF)=="n"):which(colnames(finalDF)=="norm.n")])
+    return(finalDF)
+}
+
+
+
+
+
+
+
+
+
+
 # Set all experiment directories
 directories <- c("~/Dropbox/HTA/Results/20110811_RIAILs0a", "~/Dropbox/HTA/Results/20110812_RIAILs0b", "~/Dropbox/HTA/Results/20110818_RIAILs0c", "~/Dropbox/HTA/Results/20110819_RIAILs0d")
 
@@ -44,31 +76,52 @@ directories <- c("~/Dropbox/HTA/Results/20140616_GWAS8a", "~/Dropbox/HTA/Results
 
 directories <- c("~/Dropbox/HTA/Results/20140609_GWAS7a", "~/Dropbox/HTA/Results/20140610_GWAS7b")
 
-
-
 # Get the setup list vector
-setupList <- sapply(directories,
+setupList <- sapply(unlist(sapply(directories,
                     function(x){lapply(list.files(x, pattern="\\.txt", recursive=TRUE, full.names=TRUE),
                                        function(y){if(!grepl("IncompleteData",y) & !grepl("UnstitchedData",y) & !grepl("Archive",y) & grepl("setup",y)){return(y)}})})
-setupList <- unlist(setupList)
-setupList <- sapply(setupList, as.character)
+                          ), as.character)
 
 # Get the score list vector
-scoreList <- sapply(directories,
+scoreList <- sapply(unlist(sapply(directories,
                    function(x){lapply(list.files(x, pattern="\\.txt", recursive=TRUE, full.names=TRUE),
                                       function(y){if(!grepl("IncompleteData",y) & !grepl("UnstitchedData",y) & !grepl("Archive",y) & grepl("score",y)){return(y)}})})
-scoreList <- unlist(scoreList)
-scoreList <- sapply(scoreList, as.character)
+                          ), as.character)
 
 # Read in all setup plates
 sortData <- do.call(rbind, lapply(setupList, function(x){data.frame(cbind(info(x,2), procSetup(x, 60, 2000), step="setup"))}))
 
 # Read in all score plates
 rawScoreData <- do.call(rbind, lapply(scoreList, function(x){data.frame(cbind(info(x,2), readPlate(x, 60, 2000), step="score"))}))
-summarizedScoreData <- rawScoreData %>% group_by(date, experiment, round, assay, plate, drug) %>% do(summarizePlate(., quantiles=TRUE))
 
+# Get the strain data
+strainFiles <- sapply(unlist(sapply(directories,
+                                    function(x){
+                                        lapply(list.files(x,
+                                                          pattern="\\.R",
+                                                          recursive=TRUE,
+                                                          full.names=TRUE),
+                                               function(y){
+                                                   if(!grepl("IncompleteData",y) & !grepl("UnstitchedData",y) & !grepl("Archive",y) & grepl("strains",y)){
+                                                       return(y)
+                                                   }
+                                               })
+                                    })), as.character)
+
+strainsData <- do.call(rbind, lapply(strainFiles, function(x){
+                                                    source(x)
+                                                    data.frame(cbind(assay=as.character(info(x)$assay),
+                                                        I(list(strains))))
+                                                  }))
+colnames(strainsData) <- c("assay", "strains")
+
+# Summarize the score data
+summarizedScoreData <- rawScoreData %>% group_by(date, experiment, round, assay, plate, drug) %>% do(summarizePlate(., strains=eval(strainsData[strainsData$assay==.$assay[1],2])[[1]], quantiles=TRUE))
+
+# Join the number sorted and calculate norm.n, then remove the number sorted 
 completeData <- left_join(summarizedScoreData, select(sortData, assay, plate, row, col, n.sorted.setup = n.sorted)) %>% mutate(norm.n=n/n.sorted.setup) %>% select(-contains("sort"))
 
+#Read in and handle contamination
 contamFiles <- sapply(unlist(sapply(directories,
                                     function(x){
                                         lapply(list.files(x,
@@ -89,63 +142,27 @@ colnames(contamination) <- c("assay", "plate", "contam")
 contamination$plate <- sapply(contamination$plate, function(x){as.numeric(strsplit(as.character(x), "p")[[1]][2])})
 contamination$contam <- I(sapply(contamination$contam, function(x){eval(parse(text=gsub("- ", "", as.character(x))))}))
 
+#Remove contamination
 completeData <- completeData %>% group_by(assay, plate) %>% do(removeWells(., unlist(contamination[contamination$assay==.$assay[1] & contamination$plate==as.numeric(.$plate[1]),3])))
 
 #NA out wash wells, change if patterning is different than for mapping
-completeData[as.numeric(completeData$col) %% 2 == 0, which(colnames(completeData)=="n"):ncol(completeData)] <- NA
+completeData[is.na(completeData$strain), which(colnames(completeData)=="n"):ncol(completeData)] <- NA
 
 #NA out any wells where norm.n is infinite (i.e. nothing sorted to well)
 completeData[is.infinite(completeData$norm.n), which(colnames(completeData)=="n"):ncol(completeData)] <- NA
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-regress <- function(data, completeData){
-    plates <- data[!duplicated(data[,c("assay", "plate")]), c("assay", "plate")]
-    do.call(rbind, apply(plates, 1, function(x){completeData %>% filter(assay==x[["assay"]], plate %in% controls[controls$assay==assay & x[["plate"]] %in% controls$plate, "control"])}))
-}
-
-
 controls <- do.call(rbind, lapply(lapply(directories, function(x){list.files(x, pattern="controls", recursive=TRUE, full.names=TRUE)}),
-                   function(x){
-                       source(x)
-                       assay <- info(x)$assay
-                       data.frame(assay, control=I(controlPlates), plates=I(testPlates))
-                   }))
+                                  function(x){
+                                      source(x)
+                                      assay <- info(x)$assay
+                                      data.frame(assay, control=I(controlPlates), plates=I(testPlates))
+                                  }))
 
-completeData %>% group_by(drug)
+finalData <- completeData %>% group_by(drug) %>% do(regress(., completeData, controls)) %>% arrange(assay)
 
-write.csv(completeData, "~/Dropbox/HTA/Results/ProcessedData/GWAS8_complete_simple.csv")
-
-
+write.csv(finalData, "~/Dropbox/HTA/Results/ProcessedData/GWAS7_complete_simple.csv")
 
 
-
-
-
-
-
-
-
-
-
-# raw3 <- do.call(rbind, lapply(fileList, function(x){data.frame(cbind(info(x,2), readPlate_worms(x, 60, 2000)))}))
-# raw2 <- do.call(rbind, lapply(fileList, function(x){data.frame(cbind(info(x,2), readPlate(x, 60, 2000)))}))
-# 
-# sum3 <- raw3 %>% group_by(assay, plate) %>% do(summarizePlate(.))
-# sum2 <- raw2 %>% group_by(assay, plate) %>% do(summarizePlate(.))
-# 
-# 
 # write.csv(summarizedData, "RIAILs0_raw2.csv")
 
-
+tester123 <- completeData %>% filter(assay=="a", drug=="control-lysate") %>% summarise_each(funs(mean(., na.rm=TRUE)))
